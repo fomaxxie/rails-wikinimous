beb828e99cf3d512e37770ff53a7bf0e5c6d3820
Maksim Fomkin <fomaxxi@gmail.com>
2023-08-15 18:02:31 +0200

    wiki

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Create
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+  post 'articles/', to: 'articles#create'
+
+  # Read
+  get 'articles', to: 'articles#index'
+  get 'articles/:id', to: 'articles#show', as: 'article'
+
+  # # Update
+  # get 'article/:id/edit', to: 'article#edit', as: 'edit_article'
+  # patch 'article/:id', to: 'article#update'
+
+  # # Delete
+  # delete 'article/:id', to: 'article#destroy'
+
 end
